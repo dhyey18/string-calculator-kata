2f8523c22710a6aa2122d535f3eea2af095f7d03
dhyey18 <pateldhyey64@gmail.com>
2024-09-30 12:07:38 +0530

    Feat: Throw error for negative numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -10,6 +10,12 @@ export class StringCalculator {
     }
   
     const numArray = numbers.replace(/\n/g, delimiter).split(delimiter).map(num => parseInt(num, 10));
+  
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+    }
+  
     return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
